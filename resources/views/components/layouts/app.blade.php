5514aabb49bf5f9cd1c12a39c0659735729dd97f
vagnerbrz <souza.vagner92@gmail.com>
2025-06-30 20:06:06 -0400

    Correção 2.0

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -15,5 +15,15 @@
     {{ $slot }}
 
     @fluxScripts
+
+    
+
+<footer class="m-4">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <span class="block text-sm text-zinc-500 sm:text-center dark:text-zinc-400"><a href="$" class="hover:underline"></a>By Infernal WarriorS // VsAll 😎 </span>
+    </div>
+</footer>
+
+
 </body>
 </html>
